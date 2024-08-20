e1604ea3112394aeabff82ca624a1b2bac1024e9
Mohan Li <mohanli9807@gmail.com>
2024-08-20 09:18:35 +0200

    original commit

--- split_fn_trDA_teDR.py
+++ split_fn_trDA_teDR.py
@@ -16,6 +16,18 @@ def set_seed(
     np.random.seed(RANDOM_SEED)
 
 def split_trDA_teDR_Px(
-        
+    train_features: torch.Tensor,
+    train_labels: torch.Tensor,
+    test_features: torch.Tensor,
+    test_labels: torch.Tensor,
+    client_number: int = 10,
+    dataset_scaling: float = 1.0,
+    rotation_bank: int = 1,
+    color_bank: int = 1,
+    epoch_locker_num: int = 10,
+    random_locker: bool = False,
+    px_scaling_low: float = 0.5,
+    px_scaling_high: float = 0.5,
+    verbose: bool = True
 ) -> list:
     pass
